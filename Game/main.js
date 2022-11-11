a665fec3e0dc8e560f5750ff5f7657a5aadc394c
Jung Tae Min <71391013+aquisto535@users.noreply.github.com>
2022-11-11 17:54:09 +0900

    update game
    make game more clearly

--- Game/main.js
+++ Game/main.js
@@ -1,17 +1,20 @@
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 
-canvas.width = window.innerWidth - 100;
+canvas.width = window.innerWidth - 650;
 canvas.height = window.innerHeight - 100;
 
 img1 = new Image();
-img1.src = "./catus2.jpg";
+img1.src = "./catus.jpg";
 img2 = new Image();
 img2.src = "./dino.png";
 
+//ctx.drawImage();
+
+//공룡 객체
 let dino = {
   x: 10,
-  y: 200,
+  y: 300,
   width: 50,
   height: 50,
 
@@ -23,10 +26,11 @@ let dino = {
 };
 dino.x += 1;
 
+//선인장 객체
 class Catus {
   constructor() {
-    this.x = 500;
-    this.y = 200;
+    this.x = 1200;
+    this.y = 300;
     this.width = 50;
     this.height = 50;
   }
@@ -37,20 +41,52 @@ class Catus {
   }
 }
 
+//점수판
+let score = 0;
+
+function drawscore() {
+  score += 1;
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "#0f2a3f";
+  ctx.fillText("Score:" + score, 50, 20);
+}
+
+//선 객체
+class Line {
+  constructor() {
+    this.x = 100;
+    this.y = 350;
+    this.width = canvas.width;
+    this.height = 1;
+  }
+  draw() {
+    ctx.fillStyle = "black";
+    ctx.fillRect(this.x, this.y, this.width, this.height);
+  }
+}
+
 let timer = 0;
 let catuses = [];
 let jump_timer = 0;
 let onJump = false;
 let animation;
+let keypressed = [];
 
 //1초에 60번 코드 실행하기. 프레임마다 실행됨. 실행횟수는 모니터 fps에 따라 다름.
 function runByFrame() {
+  ctx;
   animation = requestAnimationFrame(runByFrame); //애니매이션 생성기
   timer++;
   ctx.clearRect(0, 0, canvas.width, canvas.height); // 잔상 제거
 
+  let line = new Line();
+
+  line.draw();
+
+  drawscore();
+
   //일정주기마다 장애물 생성
-  if (timer % 200 == 0) {
+  if (timer % 300 == 0) {
     let catus = new Catus();
     catuses.push(catus); // 장애물 여러개 생성 후 관리하기.
   }
@@ -69,19 +105,32 @@ function runByFrame() {
 
   //스페이스 누를 시 점프 코드
   if (onJump == true) {
-    dino.y--;
+    dino.y -= 2; //dino.y = 300
     jump_timer++;
   }
 
-  if (jump_timer > 100) {
+  if (jump_timer > 50) {
     onJump = false;
   }
 
   if (onJump == false) {
-    if (dino.y < 200) {
-      dino.y++;
+    if (dino.y < 300) {
+      dino.y += 2;
       jump_timer = 0;
     }
+  }
+  // 공룡 왼쪽으로 사라짐 방지.
+  if (dino.x < 0) {
+    dino.x = 5;
+  }
+
+  if (keypressed[37]) {
+    //왼쪽 방향키 이동
+    dino.x = dino.x - 3;
+  }
+  if (keypressed[39]) {
+    //오른쪽 방향키 이동
+    dino.x = dino.x + 3;
   }
 
   dino.draw();
@@ -94,8 +143,14 @@ function crash(dino, catus) {
   let y_differ = catus.y - (dino.y + dino.height);
 
   if (x_differ < 0 && y_differ < 0) {
-    ctx.clearRect(0, 0, canvas.width, canvas.height); // 화면 리셋
     cancelAnimationFrame(animation);
+    ctx.fillStyle = "red";
+    ctx.font = "60px";
+    ctx.fillText(
+      "재시작하시려면 엔터를 눌러주세요",
+      canvas.width / 2 - 50,
+      canvas.height / 2 + 200
+    );
   }
 }
 
@@ -104,5 +159,22 @@ document.addEventListener("keydown", (e) => {
     onJump = true;
   }
 });
+
+document.addEventListener("keydown", (e) => {
+  if (e.code === "Enter") {
+    location.reload();
+  }
+});
+
+window.addEventListener("keydown", getKeyDown, false);
+window.addEventListener("keyup", getKeyUp, false);
+
+function getKeyDown(event) {
+  keypressed[event.keyCode] = true;
+}
+
+function getKeyUp(event) {
+  keypressed[event.keyCode] = false;
+}
 
 runByFrame();
